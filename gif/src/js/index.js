3ea2424197cc58974ec0e468628141b17fc44dc9
Samuele Girgenti <samuele.girgenti458@gmail.com>
2021-01-10 17:36:29 +0100

    Update index.js
    Co-Authored-By: Alessandro privitera <63925908+Bao500ct@users.noreply.github.com>

--- gif/src/js/index.js
+++ gif/src/js/index.js
@@ -3,7 +3,7 @@ document.getElementById("button").addEventListener("click", ()=> {
 
     let input_text = document.getElementById("input").value;
 
-    ShowGIFS(input_text);
+    Request();
 
 });
 
@@ -12,31 +12,43 @@ document.getElementById("input").addEventListener("keyup", (e)=> {
     let input_text = document.getElementById("input").value;
 
     if (e.keyCode === 13) {
-        ShowGIFS(input_text);
+       Request();
     }
 
 });
 
-let url = "http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=dc6zaTOxFJmzC";
+
 
 // AJAX Request
-let call = new XMLHttpRequest();
-call.open( 'GET', url );
-call.send();
+function Request(){
 
-call.addEventListener('load',(e)=>{
-
-    let data = e.target.response;
-    console.log(data);
-
-})
+    let url = "http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=dc6zaTOxFJmzC";
+    let call = new XMLHttpRequest();
+    call.open( 'GET', url );
+    call.send();
+    
+    
+    call.addEventListener('load',(e)=>{
+    
+        let data = JSON.parse(e.target.response);
+        
+        console.log(data);
+        ShowGIFS( data );
+    
+    });
+}
 
 
 /* 3. Show GIFs */
 
-function ShowGIFS(input_text) {
+function ShowGIFS(data) {
 
-    document.getElementById("content").innerHTML = input_text;
+
+    var imageURL = data.data[0].images.fixed_height.url;
+    console.log(imageURL);
+
+    document.getElementById('content').innerHTML = `<img src="${imageURL}">`;
+    //document.getElementById("content").innerHTML = input_text;
 
     
 }
