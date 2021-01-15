34a957adc533d33247dd6592f6016b025648135e
Samuele Girgenti <samuele.girgenti458@gmail.com>
2021-01-15 22:54:17 +0100

    Update Gif Search Engine
    Co-authored-by: Alessandro privitera <63925908+Bao500ct@users.noreply.github.com>

--- gif/src/js/index.js
+++ gif/src/js/index.js
@@ -1,6 +1,7 @@
 
 window.onload = function() {
  
+    
     const url = "http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC";
 
     let call = new XMLHttpRequest();
@@ -57,7 +58,9 @@ function Request(query) {
 
         let data = JSON.parse(e.target.response);
 
-        console.log(data);
+        document.getElementById('content').innerHTML = "";
+
+        console.log(data.length);
         ShowGIFS(data);
 
     });
@@ -67,45 +70,59 @@ function Request(query) {
 /* 3. Show GIFs */
 
 function ShowGIFS(data) {
-    document.getElementById('content').innerHTML = "";
 
-    let color = ['primary','secondary', 'success','info','warning', 'danger', 'dark' ] //color card array
+    if(data.data.length == 0){
 
-    data.data.forEach(image => {
-        let imageURL = image.images.fixed_height.url;
-        const title = image.title;
-        const capitalTitle = title.replace(/^\w/, e => e.toUpperCase()); // capitalize the first letter of a string
-
-        let rand = Math.floor((Math.random() * color.length)); // generate a random value to determine the color of the card
+        document.getElementById('content').innerHTML = `<div class="text-center h1 ">Gif not found</div> `;;
 
 
+    } else {
 
-        document.getElementById('content').innerHTML += `
-        <div class="col-md-3 hover-img">
-        <div class="card text-white bg-${color[rand]}  mb-3">
-            <div class="card-header h5">${capitalTitle}</div>
-            <div class="card-body p-1">
-                <img src="${imageURL}" class="card-img-top" alt="">
-            </div>
-            <div class="card-footer p-1 text-muted">
-                <div class="d-flex">
-                    <input class="form-control form-control-sm rounded-0 " id="input" type="search"
-                        value="${imageURL}" aria-label="Search" readonly>
-                    <button type="button sm" class="copy-gif-button btn btn-primary rounded-0  btn-sm"><i
-                            class="fas  fa-share-alt-square"></i></button>
+
+        let color = ['primary','secondary', 'success','info','warning', 'danger', 'dark' ] //color card array
+
+        data.data.forEach(image => {
+
+
+            let data = image.import_datetime;
+            let imageURL = image.images.fixed_height.url;
+            const title = image.title;
+            const capitalTitle = title.replace(/^\w/, e => e.toUpperCase()); // capitalize the first letter of a string
+    
+            let rand = Math.floor((Math.random() * color.length)); // generate a random value to determine the color of the card
+    
+    
+    
+            document.getElementById('content').innerHTML += `
+            <div class="col-md-3 hover-img">
+            <div class="card text-white bg-${color[rand]}  mb-3">
+                <div class="card-header h5">${capitalTitle}</div>
+                <div class="card-body p-1">
+                    <img src="${imageURL}" class="card-img-top" alt="">
                 </div>
-                <span class="h6 text-white">2 days ago</span>
-                <div class="col">
+                <div class="card-footer p-1 text-muted d-none">
+                    <div class="d-flex">
+                        <input class="form-control form-control-sm rounded-0 " id="input" type="search"
+                            value="${imageURL}" aria-label="Search" readonly>
+                        <button type="button sm" class="copy-gif-button btn btn-primary rounded-0  btn-sm"><i
+                                class="fas  fa-share-alt-square"></i></button>
+                    </div>
+                    <span class="h6 text-white">${date}</span>
+                    <div class="col">
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-    `;
-    });
+        `;
+        });
+        
+    }
+
 
     document.querySelectorAll('.hover-img').forEach(e => {
 
         e.addEventListener("mouseover", function (event) {
+            console.log(this);
             this.firstElementChild.lastElementChild.classList.remove("d-none");
 
         });
@@ -115,13 +132,11 @@ function ShowGIFS(data) {
             }
         });
 
-
     });
 
     document.querySelectorAll('.copy-gif-button').forEach(element => {
         element.addEventListener("click", function () {
-            console.log(this);
-            UrlInput = this.parentElement.firstElementChild;
+            let UrlInput = this.parentElement.firstElementChild;
             UrlInput.select();
             UrlInput.setSelectionRange(0, 999);
             document.execCommand("copy");
